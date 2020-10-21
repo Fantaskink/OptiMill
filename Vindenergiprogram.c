ed4bea2cd36de823d63832e4a830fc55e7ea5f18
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2020-10-21 09:26:06 +0200

    Update Vindenergiprogram.c

--- Vindenergiprogram.c
+++ Vindenergiprogram.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <math.h>
+#define MAX_LEN 128
+
+void print_image(FILE *fptr);
 
 int main(void)
 {
-    printf("Hello Github");
+    printf("Hello Github\n");
+
+    char *filename = "image.txt";
+    FILE *fptr = NULL;
+ 
+    if((fptr = fopen(filename,"r")) == NULL)
+    {
+        fprintf(stderr,"error opening %s\n",filename);
+        return 1;
+    }
+ 
+    print_image(fptr);
+ 
+    fclose(fptr);
+ 
     return 0;
+}
+
+void print_image(FILE *fptr)
+{
+    char read_string[MAX_LEN];
+ 
+    while(fgets(read_string,sizeof(read_string),fptr) != NULL)
+        printf("%s",read_string);
 }
