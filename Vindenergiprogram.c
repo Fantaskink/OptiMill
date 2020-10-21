dc8b967d7b50891f2726c8fda37da42831473a51
Jakob Saadbye <jsaadbye@h194.aau1x-3.klient.sydhavn2.site.aau.dk>
2020-10-21 10:24:58 +0200

    Added function to calculate data(test)

--- Vindenergiprogram.c
+++ Vindenergiprogram.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
+calc_data(double vh);
+
 int main(void)
 {
-    printf("Hello Github");
-    printf("Test123");
+    double data = 2.0;
     return 0;
+    printf("Vind hastigheden er: %f",get_data(data));
+}
+
+calc_data(double vh) {
+
+    return vh;
 }
