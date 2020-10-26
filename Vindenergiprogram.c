2e4ad24aad2771390d4f7ff37e9b26374d8a0c81
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2020-10-26 10:11:42 +0100

    Tilføjet region

--- Vindenergiprogram.c
+++ Vindenergiprogram.c
@@ -3,10 +3,21 @@
 #include <string.h>
 #define MAX_LEN 128
 
+//enumerators
+typedef enum region 
+{
+    Hovedstaden, 
+    Sydjylland, 
+    Nordjylland, 
+    Midtjylland, 
+    Sjaelland
+} region;
+
 //Structs
 struct Area
 {
     char name[50];
+    region region;
     double wind_speed; //Meters/second
     int inSea;
     double land_height; //in meters
@@ -30,13 +41,16 @@ void print_image(FILE *fptr);
 double calc_data(double vh);
 void print_area(struct Area area);
 double calc_area_expenses(struct Area area);
+const char* get_region(struct Area area);
 
 int main(void)
 {
     struct Area Omraade1;
 
-    strcpy( Omraade1.name, "Meadow near Copenhagen");
+    strcpy( Omraade1.name, "København");
+
     Omraade1.wind_speed = 4;
+    Omraade1.region = 0;
     Omraade1.inSea = 0;
     Omraade1.land_height = 2;
     Omraade1.tree_factor = 0;
@@ -76,7 +90,9 @@ void print_image(FILE *fptr)
 
 void print_area(struct Area area)
 {
+
     printf("name: %s\n", area.name);
+    printf("Region: %s\n", get_region(area)); 
     printf("Wind Speed: %.2f\n", area.wind_speed);
     printf("InSea: %d\n", area.inSea);
     printf("Expenses: %.2f\n", area.expenses);
@@ -92,4 +108,29 @@ double calc_area_expenses(struct Area area)
     (area.inSea * 1000000);
 
     return(expenses);
+}
+
+const char* get_region(struct Area area)
+{
+        switch (area.region)
+    {
+    case 0:
+        return("Hovedstaden");
+        break;
+    case 1:
+        return("Sydjylland");
+        break;
+    case 2:
+        return("Nordjylland");
+        break;
+    case 3:
+        return("Midtjylland");
+        break;
+    case 4:
+        return("Sjaelland");
+        break;
+    default:
+        return("Unknown region");
+        break;
+    }
 }
